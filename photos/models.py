039acc8462fda591f83ef754a7c34b99ffdfc609
setusher <shradhath08@gmail.com>
2025-12-11 10:55:56 +0530

    add photos model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from events.models import Event
+from django.conf import settings 
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Photo(models.Model):
+    
+    #relationships
+    album = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='photos')
+    uploaded_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='photos_uploaded')
+
+    #files
+    image = models.ImageField(upload_to='photos/')
+    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
+
+    #metadata
+    caption = models.CharField(max_length=255, blank=True)
+    tags = models.JSONField(default=list, blank=True)
+
+    width = models.PositiveIntegerField(null=True, blank=True)
+    height = models.PositiveIntegerField(null=True, blank=True)
+    exif_data = models.JSONField(null=True, blank=True)
+
+    is_approved = models.BooleanField(default=True)
+    is_public = models.BooleanField(default=True)
+
+    processing_status = models.CharField(
+        max_length=20,
+        choices=[
+            ("pending", "Pending"),
+            ("processing", "Processing"),
+            ("done", "Done"),
+            ("failed", "Failed")
+        ],
+        default="pending"
+    )
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Photo {self.id} in {self.album.name}"
