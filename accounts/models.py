d33822c257963bb02c48ee9c87a20ad65cc70e22
setusher <shradhath08@gmail.com>
2025-12-11 09:32:16 +0530

    define user

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUsermanager
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
 
 class UserManager(BaseUserManager):
-    def create_user(self email, password=None, **extra_fields):
+    def create_user(self ,email, password=None, **extra_fields):
         if not email:
             raise ValueError('You have not provided an email')
         
@@ -12,10 +12,40 @@ class UserManager(BaseUserManager):
         user.save()
         return user
 
-    def create_superuser(self email, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         
         
         return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser, PermissionsMixin):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('coordinator', 'EVent Coordinator'),
+        ('photographer', 'Photographer'),
+        ('student', 'Student'),
+        
+    )
+
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    profile_picture = models.ImageField(upload_to='profiles/', blank = True, null = True)
+    
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+    
+    batch = models.CharField(max_length = 10, blank = True, null = True)
+    department = models.CharField(max_length = 50, blank = True, null = True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return f"{self.email}-{self.role}"
 
