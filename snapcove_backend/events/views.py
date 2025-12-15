5ecbc4b05ee66a919f71e37a446098d58f8156d2
setusher <shradhath08@gmail.com>
2025-12-15 14:27:55 +0530

    add events view

--- snapcove_backend/events/views.py
+++ snapcove_backend/events/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .models import Event, Album
+from serializers import EventSerializer, AlbumSerializer
 
-# Create your views here.
+class EventViewSet(viewsets.ModelViewSet):
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        #logged in
+        if user.is_authenticated:
+            return Event.objects.all()
+        
+        #not logged in
+        return Event.objects.filter(is_public=True)
+    
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+
