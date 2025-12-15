a3c188abe49208bfbf3db36dbf5db5c09eddf129
setusher <shradhath08@gmail.com>
2025-12-15 14:31:51 +0530

    add album view

--- snapcove_backend/events/views.py
+++ snapcove_backend/events/views.py
@@ -21,4 +21,18 @@ class EventViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
 
+class AlbumViewSet(viewsets.ModelViewSet):
+    serializer_class = AlbumSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        #logged in
+        if user.is_authenticated:
+            return Album.objects.all()
+        
+        #not logged in
+        return Album.objects.filter(is_public=True)
+
 
