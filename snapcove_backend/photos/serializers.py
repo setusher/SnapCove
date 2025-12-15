054dcfee86af50f184a0318fc0099171f1d763d0
setusher <shradhath08@gmail.com>
2025-12-15 21:55:30 +0530

    add photo serializer

---
+++ snapcove_backend/photos/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Photo
+
+class PhotoSerializer(serializers.ModelSerializer):
+    uploaded_by = UserMiniSerializer(read_only=True)
+
+    class Meta:
+        model = Photo
+        fields = '__all__'
+        read_only_fields = ['id', 'uploaded_by', 'processing_status','created_at', 'updated_at']
+    
