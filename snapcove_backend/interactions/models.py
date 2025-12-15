2713cee5cd746016ae484359d6017afe94ad7f5f
setusher <shradhath08@gmail.com>
2025-12-15 12:34:56 +0530

    add notif and interaction models

--- snapcove_backend/interactions/models.py
+++ snapcove_backend/interactions/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Like(models.Model):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    photo = models.ForeignKey('photos.Photo', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Like {self.id} by {self.user.email} on {self.photo.id}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    photo = models.ForeignKey('photos.Photo', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment {self.id} by {self.user.email} on {self.photo.id}"
--- snapcove_backend/notifications/models.py
+++ snapcove_backend/notifications/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Notification(models.Model):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
