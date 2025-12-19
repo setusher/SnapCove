614731eab69f54cde0a08d0bb8a0b3e3aae68d7e
setusher <shradhath08@gmail.com>
2025-12-20 01:52:07 +0530

    add signals

---
+++ snapcove_backend/notifications/signals.py
@@ -0,0 +1,54 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Notification
+from interactions.models import Comment, Like
+
+@receiver(post_save, sender=Like)
+def notify_like(sender, instance,created, **kwargs):
+    if not created:
+        return
+    
+    photo = instance.photo
+    recipient = photo.uploaded_by
+    actor = instance.user
+
+    if recipient == actor:
+        return
+
+    Notification.objects.create(
+        recipient=recipient,
+        actor=actor,
+        notification_type='like',
+        photo=photo,
+        message=f"{actor.name} liked your photo",
+    )
+
+
+@receiver(post_save, sender=Comment)
+def notify_comment(sender, instance,created, **kwargs):
+    if not created:
+        return
+
+    actor = instance.user
+    
+    if instance.parent:
+        recipient = instance.parent.user
+        if recipient != actor:
+            Notification.objects.create(
+                recipient=recipient,
+                actor=actor,
+                notification_type='reply',
+                comment = instance,
+                message=f"{actor.name} replied to your comment",
+            )
+        
+        else:
+            recipient = instance.photo.uploaded_by
+            if recipient != actor:
+                Notification.objects.create(
+                    recipient=recipient,
+                    actor=actor,
+                    notification_type='comment',
+                    comment = instance,
+                    message=f"{actor.name} commented on your photo",
+                )
