810186de6b0915473c02202ac0b4fbabe90727b0
setusher <shradhath08@gmail.com>
2025-12-20 22:17:31 +0530

    media file config

--- snapcove_backend/snapcove_backend/settings.py
+++ snapcove_backend/snapcove_backend/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
