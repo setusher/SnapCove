964321e86f101e1d0f3f36a0b72309de5e5fde31
setusher <shradhath08@gmail.com>
2025-12-24 23:41:06 +0530

    views

--- snapcove_backend/accounts/dashboard_view.py
+++ snapcove_backend/accounts/dashboard_view.py
@@ -12,3 +12,11 @@ from events.models import Event, Album
 from photos.models import Photo
 from interactions.models import Like, Comment
 from notifications.models import Notification
+
+class AdminDashboardView(APIView):
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get(self, request):
+        total_users = User.object.count()
+        users_by_role = User.objects.values('role').annotate(count=Count('id'))
+        recent_users = User.objects.order_by('-date_joined')[:5].values
