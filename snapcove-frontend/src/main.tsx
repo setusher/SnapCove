d8194ef7dd0f3a0cb2c40269d8bab6c28eb63476
setusher <shradhath08@gmail.com>
2025-12-28 15:00:52 +0530

    fix

--- snapcove-frontend/src/main.tsx
+++ snapcove-frontend/src/main.tsx
@@ -4,7 +4,11 @@ import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import "./index.css"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root")
+
+if (!root) throw new Error("Root element not found")
+
+ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
