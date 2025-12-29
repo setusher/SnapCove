37ff45080c9dbe6e74f7f5005e13e003419a9cf3
setusher <shradhath08@gmail.com>
2025-12-30 02:47:13 +0530

    add basic frontend pages

--- snapcove-frontend/src/App.tsx
+++ snapcove-frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from "react-router-dom"
 import Landing from "./pages/Landing"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
+      <Route path="/" element={<Landing/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
     </Routes>
   )
 }
