5b658ddf703c06a129d21f6ee5e876eba353211f
setusher <shradhath08@gmail.com>
2025-12-30 14:52:35 +0530

    Change design

--- snapcove_frontend/src/App.jsx
+++ snapcove_frontend/src/App.jsx
@@ -4,9 +4,8 @@ import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import Dashboard from "./pages/Dashboard"
 import ProtectedRoute from "./auth/ProtectedRoute"
-import CreateEvent from "./pages/CreateEvent"
 import RoleGate from "./auth/RoleGate"
-// import CreateEvent from "./pages/CreateEvent"
+import CreateEvent from "./pages/CreateEvent"
 import EventDetail from "./pages/EventDetail"
 import CreateAlbum from "./pages/CreateAlbum"
 import AlbumGallery from "./pages/AlbumGallery"
@@ -17,11 +16,13 @@ export default function App() {
       <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+
       <Route path="/dashboard" element={
-      <ProtectedRoute>
-        <Dashboard/>
-      </ProtectedRoute>
+        <ProtectedRoute>
+          <Dashboard/>
+        </ProtectedRoute>
       }/>
+
       <Route path="/events/create" element={
         <ProtectedRoute>
           <RoleGate allow={["admin","coordinator"]}>
@@ -29,10 +30,10 @@ export default function App() {
           </RoleGate>
         </ProtectedRoute>
       }/>
-      <Route path="/events/:id" element={<ProtectedRoute><EventDetail/></ProtectedRoute>} />
-<Route path="/events/:id/albums/create" element={<ProtectedRoute><CreateAlbum/></ProtectedRoute>} />
-<Route path="/events/:eventId/albums/:albumId" element={<ProtectedRoute><AlbumGallery/></ProtectedRoute>} />
 
+      <Route path="/events/:eventId" element={<ProtectedRoute><EventDetail/></ProtectedRoute>} />
+      <Route path="/events/:eventId/albums/create" element={<ProtectedRoute><CreateAlbum/></ProtectedRoute>} />
+      <Route path="/events/:eventId/albums/:albumId" element={<ProtectedRoute><AlbumGallery/></ProtectedRoute>} />
     </Routes>
   )
 }
