9b49e06547c4d3ae70b3d8cd4506a32dd4f7aa4f
setusher <shradhath08@gmail.com>
2026-01-05 13:32:13 +0530

    add downloads model

--- snapcove_backend/photos/models.py
+++ snapcove_backend/photos/models.py
@@ -52,3 +52,11 @@ class PhotoTag(models.Model):
 
     class Meta:
         unique_together = ("photo", "user")
+
+class PhotoDownload(models.Model):
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    downloaded_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("photo", "user")
